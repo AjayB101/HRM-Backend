8d29461e0a895d462ef40c46a59a305ce3f77c54
AjayB101 <134268911+AjayB101@users.noreply.github.com>
2023-06-21 10:12:00 +0530

    Employee module (#1)
    * post-employee
    
    * update completed
    
    * getById
    
    ---------
    
    Co-authored-by: AJAY-751 <ajay.b.civil.2019@snsct.org>

--- controllers/EmployeeController.js
+++ controllers/EmployeeController.js
@@ -9,6 +9,70 @@ const getEmployee=async(req,res)=>{
     res.status(500).json({error})
    }
 }
+//to create employee
+const createEmployee=async(empData,res)=>{
+   try {
+    const EmployeeCreatedData=new employeeModel({
+        ...empData
+       })
+   await EmployeeCreatedData.save().then(data=>{
+      res.status(200).json({message:'Employee created successfully',data})
+   }).catch((err)=>res.status(400).json(err))
+   } catch (error) {
+    res.status(500).json(error)
+    console.log(error)
+   }
+}
+//to delete employee
+const deleteEmployee=async(req,res)=>{
+    const{id}=req.params
+    const employee=employeeModel.findById(id).exec()
+    if(!employee){
+       return res.status(400).json({message:`user is not found`})
+    }
+    try {
+        await employeeModel.deleteOne({_id:id}).then(data=>{
+            res.status(200).json({message:`Employye is deleted having the id ${id}`,data})
+        }).catch((err)=>res.status(400).json(err))
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+//to update employee details
+const updateEmployee=async(req,res)=>{
+    
+    const employee=await employeeModel.findById(req.params.id).exec()
+    if(!employee){
+        return res.status(400).json({message:`user is not found`})
+    }
+    try {
+       await  employeeModel.findByIdAndUpdate(req.params.id,{$set:req.body}).then(data=>{
+        res.status(200).json({message:`Data is updated for the employee having th id ${req.params.id}`,data})
+       }).catch((err)=>res.status(400).json(err))
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+//getEmployeeByID
+const getEmployeeByID=async(req,res)=>{
+    const employee=await employeeModel.findById(req.params.id).exec()
+    if(!employee){
+        return res.status(400).json({message:`user is not found`})
+    }
+    try {
+        await employeeModel.findOne({_id:req.params.id}).exec().then(data=>{
+            res.status(200).json({message:`employee is found having id ${req.params.id}`,data})
+        }).catch((err)=>{
+            res.status(err)
+        })
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
 module.exports={
-    getEmployee
+    getEmployee,
+    createEmployee,
+    deleteEmployee,
+    updateEmployee,
+    getEmployeeByID
 }
--- router/EmployeeRouter.js
+++ router/EmployeeRouter.js
@@ -1,7 +1,19 @@
 const express=require('express')
-const { getEmployee } = require('../controllers/EmployeeController')
+const { getEmployee,createEmployee,deleteEmployee,updateEmployee,getEmployeeByID } = require('../controllers/EmployeeController')
 const router=express.Router()
 router.get('/getEmployee',async(req,res)=>{
     await getEmployee(req,res)
+})
+router.post('/createEmployee',async(req,res)=>{
+    await createEmployee(req.body,res)
+})
+router.delete('/getEmployee/:id',async(req,res)=>{
+await deleteEmployee(req,res)
+})
+router.put('/getEmployee/:id',async(req,res)=>{
+    await updateEmployee(req,res)
+})
+router.get('/getEmployee/:id',async(req,res)=>{
+    await getEmployeeByID(req,res)
 })
 module.exports=router
