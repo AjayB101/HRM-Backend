d6818201ff7fc1a75aed53c381e2c8bfb9de24e1
AJAY-751 <ajay.b.civil.2019@snsct.org>
2023-06-24 12:26:49 +0530

    getByID

--- controllers/RecruitmentController.js
+++ controllers/RecruitmentController.js
@@ -11,50 +11,83 @@ const getRec = async (req, res) => {
     res.status(500).json(error);
   }
 };
-const createRec=async(req,res)=>{
+const createRec = async (req, res) => {
   try {
-    const recData=new recModel({
-      ...req
-    })
-    await recData.save().then(data=>{
-      res.status(200).json({message:`The rec data has been added successfully`,data})
-    }).catch((err)=>res.status(400).json(err))
+    const recData = new recModel({
+      ...req,
+    });
+    await recData
+      .save()
+      .then((data) => {
+        res
+          .status(200)
+          .json({ message: `The rec data has been added successfully`, data });
+      })
+      .catch((err) => res.status(400).json(err));
   } catch (error) {
-    res.status(500).json(error)
+    res.status(500).json(error);
   }
-}
+};
 
-const deleteRec=async(req,res)=>{
-  const {id}=req.params
-  const user=recModel.findById(id).exec()
-  if(!user){
-    res.status(400).json({message:`There is no user with id ${id}`})
+const deleteRec = async (req, res) => {
+  const { id } = req.params;
+  const user = recModel.findById(id).exec();
+  if (!user) {
+    res.status(400).json({ message: `There is no user with id ${id}` });
   }
   try {
-    await recModel.deleteOne({_id:id}).then(data=>{
-      res.status(200).json({message:`deleted the user with id ${id}`,data})
-    }).catch((err)=>res.status(400).json(err))
+    await recModel
+      .deleteOne({ _id: id })
+      .then((data) => {
+        res
+          .status(200)
+          .json({ message: `deleted the user with id ${id}`, data });
+      })
+      .catch((err) => res.status(400).json(err));
   } catch (error) {
-    res.status(500).json(error)
+    res.status(500).json(error);
   }
-}
-const updateRec=async(req,res)=>{
-const {id}=req.params
-const user=recModel.findById(id).exec()
-if(!user){
-  res.status(400).json({message:`There is no user with id ${id}`})
-}
-try {
-   await recModel.findByIdAndUpdate(id,{$set:req.body}).then(data=>{
-    res.status(200).json({message:`User is updated having iid ${id}`,data})
-   }).catch(err=>res.status(400).json(err))
-} catch (error) {
-  res.status(500).json(error)
-}
-}
-module.exports={
+};
+const updateRec = async (req, res) => {
+  const { id } = req.params;
+  const user = recModel.findById(id).exec();
+  if (!user) {
+    res.status(400).json({ message: `There is no user with id ${id}` });
+  }
+  try {
+    await recModel
+      .findByIdAndUpdate(id, { $set: req.body })
+      .then((data) => {
+        res
+          .status(200)
+          .json({ message: `User is updated having iid ${id}`, data });
+      })
+      .catch((err) => res.status(400).json(err));
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+const getRecByID = async (req, res) => {
+  const { id } = req.params;
+  const user = recModel.findById(id).exec();
+  if (!user) {
+    res.status(400).json({ message: `There is no user with id ${id}` });
+  }
+  try {
+    await recModel
+      .findOne({ _id: id })
+      .then((data) => {
+      res.status(200).json({message:`The user is found`,data})
+      })
+      .catch((err) => res.status(400).json(err));
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+module.exports = {
   getRec,
   createRec,
   deleteRec,
-  updateRec
-}
+  updateRec,
+  getRecByID,
+};
--- router/RecruitmentRouter.js
+++ router/RecruitmentRouter.js
@@ -1,5 +1,5 @@
 const express=require('express')
-const{getRec,createRec,deleteRec,updateRec}=require('../controllers/RecruitmentController')
+const{getRec,createRec,deleteRec,updateRec,getRecByID}=require('../controllers/RecruitmentController')
 const router=express.Router()
 router.get('/getRec',async(req,res)=>{
     await getRec(req,res)
@@ -12,5 +12,8 @@ router.delete('/getRec/:id',async(req,res)=>{
 })
 router.put('/getRec/:id',async(req,res)=>{
     await updateRec(req,res)
+})
+router.get('/getRec/:id',async(req,res)=>{
+    await getRecByID(req,res)
 })
 module.exports=router
