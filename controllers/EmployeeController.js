955b244954261942b8e88448cd59cc061c1ae24d
Kannan <72759290+Kannan-S-Official@users.noreply.github.com>
2023-07-04 13:17:32 +0530

    web mail adding

--- controllers/EmployeeController.js
+++ controllers/EmployeeController.js
@@ -1,4 +1,4 @@
-// controllers/employeeController.js
+//controllers/employeeController.js
 const Employee = require('../model/Employee');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
@@ -9,7 +9,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail', // e.g., 'gmail'
   auth: {
     user: 'kannasn003@gmail.com',
-    pass: 'jpfvlsekorzehqjp',
+    pass: "jpfvlsekorzehqjp",
   },
 });
 
@@ -43,7 +43,7 @@ const getEmployeeById = async (req, res) => {
     const { id } = req.params;
     const employee = await Employee.findById(id);
     if (!employee) {
-      return res.status(404).json({ message: 'Employee not found' });
+      return res.status(404).json({ message: 'employee not found' });
     }
     res.status(200).json(employee);
   } catch (error) {
@@ -78,21 +78,9 @@ const createEmployee = async (req, res) => {
     const emailText = `Dear ${name},\n\nWelcome to SNS Square Consultancy Services Pvt Ltd!\n\nWe are delighted to have you on board. As part of your onboarding, we are pleased to provide you with the credentials for our Task Management Tool:\n\nEmail:${email}\nUser ID: ${employeeid}\nPassword: ${password}\n\nTo access the tool,This tool will help you organize and track your tasks efficiently.\n\nWe're excited to work with you and wish you a successful journey at SNS Square Consultancy Services Pvt Ltd!\n\nBest regards,\n\nHR Team\nSNS Square Consultancy Services Pvt Ltd`;
     sendEmail(email, emailSubject, emailText);
 
-    // Send WhatsApp notification
-    const twilioAccountSid = 'AC61c22964a61910b92c8174002c7b251e';
-    const twilioAuthToken = 'fa333bfb9273eeb58e0b4071949baa9d';
-    const twilioMessagingServiceSid = 'your-messaging-service-sid'; // Replace with your Messaging Service SID
-    const client = require('twilio')(twilioAccountSid, twilioAuthToken);
-    const whatsappMessage = `Dear ${name},\n\nWelcome to SNS Square! Your employee ID is ${employeeid}.`;
-    client.messages.create({
-      body: whatsappMessage,
-      messagingServiceSid: twilioMessagingServiceSid,
-      to: `whatsapp:${mob}` // assuming `mob` is the employee's mobile number
-    }).then((message) => console.log('WhatsApp message sent successfully:', message.sid));
-
     res.status(201).json('Employee created');
   } catch (error) {
-    console.error('Error creating employee:', error.message);
+    console.error('Error creating product:', error.message);
     res.status(500).json({ message: 'Internal server error' });
   }
 };
@@ -134,11 +122,11 @@ const deleteEmployee = async (req, res) => {
     const { id } = req.params;
     const employee = await Employee.findByIdAndDelete(id);
     if (!employee) {
-      return res.status(404).json({ message: 'Employee not found' });
+      return res.status(404).json({ message: 'Product not found' });
     }
     res.status(200).json(employee);
   } catch (error) {
-    console.error('Error deleting employee:', error.message);
+    console.error('Error deleting product:', error.message);
     res.status(500).json({ message: 'Internal server error' });
   }
 };
@@ -149,7 +137,7 @@ const signin = async (req, res) => {
     const user = await Employee.findOne({ email });
 
     if (!user) {
-      return res.status(401).json({ error: 'User does not exist. Please register first' });
+      return res.status(401).json({ error: 'user not exist please go and register' });
     }
 
     const validPassword = await bcrypt.compare(password, user.password);
@@ -158,10 +146,10 @@ const signin = async (req, res) => {
     }
     const secretKey = require('crypto').randomBytes(64).toString('hex');
     const token = jwt.sign({ userId: user._id }, secretKey, { expiresIn: '2d' })
-    res.status(200).json({ message: 'User signed in successfully', token, userId: user._id });
+    res.status(200).json({ message: 'User signed in successfully', token, secretKey });
   } catch (error) {
-    console.error('Error signing in:', error.message);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error('Error in signin:', error);
+    res.status(500).json({ error: 'Internal server error' });
   }
 };
 
