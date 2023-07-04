4d4e706de2e821b18981d0bc7d6316b2b281c15d
AjayB101 <ajayjadec@gmail.com>
2023-07-04 14:18:19 +0530

    Update Ats.js

--- model/Ats.js
+++ model/Ats.js
@@ -52,7 +52,7 @@ const atsSchema = new mongoose.Schema(
       required: [true, "Please Select the Year of Passing"],
     },
     skills: {
-      type: String,
+      type: [String],
       required: true,
     },
     appliedAt: {
