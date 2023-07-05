c9594d818dda742ce725b865fc86d9a985815e77
Kannan <72759290+Kannan-S-Official@users.noreply.github.com>
2023-07-05 11:54:02 +0530

    attendance model added

---
+++ controllers/AttendanceController.js
@@ -0,0 +1,27 @@
+const Attendance = require('../model/AttendanceModel');
+
+exports.checkIn = async (req, res) => {
+  try {
+    const currentTime = new Date().toLocaleTimeString();
+    const attendance = new Attendance({ checkInTime: currentTime });
+    await attendance.save();
+    res.status(200).send('Checked in successfully');
+  } catch (error) {
+    console.error('Error saving check-in time:', error);
+    res.status(500).send('Error saving check-in time');
+  }
+};
+
+exports.checkOut = async (req, res) => {
+  try {
+    const currentTime = new Date().toLocaleTimeString();
+    await Attendance.findOneAndUpdate(
+      { checkOutTime: { $exists: false } },
+      { checkOutTime: currentTime }
+    );
+    res.status(200).send('Checked out successfully');
+  } catch (error) {
+    console.error('Error updating check-out time:', error);
+    res.status(500).send('Error updating check-out time');
+  }
+};
