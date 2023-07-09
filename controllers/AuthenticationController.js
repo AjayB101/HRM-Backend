ff03b0566dd6b8204398d326283d1d688ce2f9ba
AjayB101 <ajayjadec@gmail.com>
2023-07-09 14:34:26 +0530

    commnts added

--- controllers/AuthenticationController.js
+++ controllers/AuthenticationController.js
@@ -1,7 +1,10 @@
+/* ================================================ import statements ============================================================*/
 const authModel = require("../model/Authentication");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-// * Signup * //
+
+// * ====================================    Signup    ===========================================================================* //
+
 const signup = async (req, res) => {
   const { firstname, lastname, email, password } = req.body;
   const user = await authModel.findOne({ email });
@@ -24,7 +27,7 @@ const signup = async (req, res) => {
     res.status(500).json(error);
   }
 };
-// * Login * //
+// *=============================     Login     =========================================================================================//
 
 const login = async (req, res) => {
   const { email, password } = req.body;
@@ -70,80 +73,95 @@ const login = async (req, res) => {
     res.status(500).json(error);
   }
 };
-// * To verify Tokem * //
+// * ==========================       To verify Token *==================================================================================/
+
 const verifyToken = async (req, res, next) => {
   const cookies = req.headers.cookie;
-  if(!cookies){
-    return res.status(400).json({message:'cookies expired please login again'})
+  if (!cookies) {
+    return res
+      .status(400)
+      .json({ message: "cookies expired please login again" });
   }
   console.log(`cookies= ${cookies}`);
   const prevToken = cookies.split("=")[1];
   console.log(`prevToken= ${prevToken}`);
-  if(!prevToken){
-    res.status(400).json({message:`Cookies not found`})
+  if (!prevToken) {
+    res.status(400).json({ message: `Cookies not found` });
   }
   //* if cookies present then verify the cookies *//
-  jwt.verify(prevToken,process.env.JWT_SECRET_KEY,(err,user)=>{
-    if(err){
-        res.status(400).json({message:`Unable to verify the token `})
+  jwt.verify(prevToken, process.env.JWT_SECRET_KEY, (err, user) => {
+    if (err) {
+      res.status(400).json({ message: `Unable to verify the token ` });
     }
-    req.id=user.id
-    console.log(`REQ.id = ${req.id}`)
-  })
-   //* hand th control of the code to next middleware  *//
-    next()
+    req.id = user.id;
+    console.log(`REQ.id = ${req.id}`);
+  });
+  //* handover  the control of the code to next middleware  *//
+  next();
 };
- //* to  getUser only after the tokens are verified  *//
- const getuser=async(req,res)=>{
-const userId=req.id
-console.log(`userId=\n ${userId}`)
-try {
-    const user=await authModel.findById(userId,'-password')
-    console.log(`user data -= \n ${user}`)
-    res.status(200).json({message:`User is found having the Id : ${userId}`,user})
-} catch (error) {
-    res.status(400).json(error)
-}
- }
- /* to regenerate token gain and again if the user is logged in  */
-const refreshToken=async(req,res)=>{
-    const cookies=req.headers.cookie
-    console.log(`cookies = ${cookies}`)
-    if(!cookies){
-        return res.status (400).json({message:`Token has been expired please login again`})
+//*=========================== to  getUser details only after the tokens are verified ================================================== *//
+
+const getuser = async (req, res) => {
+  const userId = req.id;
+  console.log(`userId=\n ${userId}`);
+  try {
+    const user = await authModel.findById(userId, "-password");
+    console.log(`user data -= \n ${user}`);
+    res
+      .status(200)
+      .json({ message: `User is found having the Id : ${userId}`, user });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+/* ============================== to regenerate token gain and again if the user is logged in  ====================================*/
+
+const refreshToken = async (req, res) => {
+  const cookies = req.headers.cookie;
+  console.log(`cookies = ${cookies}`);
+  if (!cookies) {
+    return res
+      .status(400)
+      .json({ message: `Token has been expired please login again` });
+  }
+  const prevToken = cookies.split("=")[1];
+  console.log(`token= ${prevToken}`);
+  jwt.verify(prevToken, process.env.JWT_SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.status(400).json(`Unable to verify tokens`);
     }
-    const prevToken=cookies.split('=')[1]
-    console.log(`token= ${prevToken}`)
-    jwt.verify(prevToken,process.env.JWT_SECRET_KEY,(err,user)=>{
-        if(err){
-           return res.status(400).json(`Unable to verify tokens`)
-        }
-         /*if no error then we clear our cookies */
-         console.log(`user.id = ${user.id}`)
-         res.clearCookie(`${user.id}`) 
-         console.log(`req.cookies= ${req.cookies[user.id]}`)
-         req.cookies[`${user.id}`]=""  
-         /* After clearing the token we need to regenerate the token again */
-         const regenerateToken=jwt.sign({id:user.id},process.env.JWT_SECRET_KEY,{
-            expiresIn:"35s"
-         })
-         console.log(`regenerateToken = \n${regenerateToken}`)
-         res.cookie(String(user.id),regenerateToken,{
-            path:'/',
-            expires:new Date(Date.now()+1000*30),
-            httpOnly:true,
-            sameSite:"lax"
-         })
-         req.id=user.id
-         console.log(`Succeccfully get  id : ${user.id}`)
-        //  next()
-    })
-}
-//*  export statements    *//
+    /*if no error then we clear our cookies */
+    console.log(`user.id = ${user.id}`);
+    res.clearCookie(`${user.id}`);
+    console.log(`req.cookies= ${req.cookies[user.id]}`);
+    req.cookies[`${user.id}`] = "";
+    /* After clearing the token we need to regenerate the token again */
+    const regenerateToken = jwt.sign(
+      { id: user.id },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "35s",
+      }
+    );
+    console.log(`regenerateToken = \n${regenerateToken}`);
+    /*creating new token on server side */
+    res.cookie(String(user.id), regenerateToken, {
+      path: "/",
+      expires: new Date(Date.now() + 1000 * 30),
+      httpOnly: true,
+      sameSite: "lax",
+    });
+    req.id = user.id;
+    console.log(`Succeccfully get  id : ${user.id}`);
+    //  next()
+  });
+};
+//*  //===============================   export statements  ==================================================================   *//
+
 module.exports = {
   signup,
   login,
   verifyToken,
   getuser,
-  refreshToken
+  refreshToken,
 };
