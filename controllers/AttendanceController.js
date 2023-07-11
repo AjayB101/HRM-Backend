8d2a47507b6d38a23bb0e0ea4a6ce18abb9017d1
Kannan <72759290+Kannan-SN@users.noreply.github.com>
2023-07-11 23:48:50 +0530

    schema changed (#34)

--- controllers/AttendanceController.js
+++ controllers/AttendanceController.js
@@ -1,4 +1,3 @@
-// backend/controllers/attendanceController.js
 const Attendance = require('../model/AttendanceModel');
 
 exports.checkIn = async (req, res) => {
@@ -25,6 +24,15 @@ exports.checkOut = async (req, res) => {
       return res.status(404).json({ message: 'No active check-in found.' });
     }
     res.status(200).json({ message: 'Check-out recorded successfully.' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+exports.getAttendance = async (req, res) => {
+  try {
+    const attendance = await Attendance.findOne(); // Retrieve the first attendance record (assuming there's only one)
+    res.status(200).json(attendance);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
--- router/AttendanceRouter.js
+++ router/AttendanceRouter.js
@@ -1,9 +1,9 @@
-// backend/routes/attendanceRoutes.js
 const express = require('express');
 const router = express.Router();
 const attendanceController = require('../controllers/AttendanceController');
 
 router.post('/checkin', attendanceController.checkIn);
 router.post('/checkout', attendanceController.checkOut);
+router.get('/', attendanceController.getAttendance);
 
 module.exports = router;
