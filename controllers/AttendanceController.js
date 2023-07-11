5cf1757a6d6f517a07f5cc4eaee763bf8bdd13bb
Kannan <72759290+Kannan-SN@users.noreply.github.com>
2023-07-11 10:29:38 +0530

    time fixed

--- controllers/AttendanceController.js
+++ controllers/AttendanceController.js
@@ -2,7 +2,7 @@ const Attendance = require('../model/AttendanceModel');
 
 exports.checkIn = async (req, res) => {
   try {
-    const currentTime = new Date().toLocaleTimeString();
+    const currentTime = new Date().toLocaleTimeString('en-IN', { timeZone: 'Asia/Kolkata' });
     const attendance = new Attendance({ checkInTime: currentTime });
     await attendance.save();
     res.status(200).send('Checked in successfully');
@@ -11,9 +11,10 @@ exports.checkIn = async (req, res) => {
     res.status(500).send('Error saving check-in time');
   }
 };
+
 exports.checkOut = async (req, res) => {
   try {
-    const currentTime = new Date().toLocaleTimeString();
+    const currentTime = new Date().toLocaleTimeString('en-IN', { timeZone: 'Asia/Kolkata' });
     await Attendance.findOneAndUpdate(
       { checkOutTime: { $exists: false } },
       { checkOutTime: currentTime }
