c2da42c7e348ef302b9c43cf7af9d70831a58197
Kannan <72759290+Kannan-SN@users.noreply.github.com>
2023-07-11 12:05:29 +0530

    database error

--- model/AttendanceModel.js
+++ model/AttendanceModel.js
@@ -3,15 +3,18 @@ const mongoose = require('mongoose');
 const attendanceSchema = new mongoose.Schema({
   checkInTime: {
     type: String,
+    required: true,
   },
   checkOutTime: {
     type: String,
   },
   employeeName: {
     type: String,
+    required: true,
   },
   employeeId: {
     type: String,
+    required: true,
   },
 });
 
