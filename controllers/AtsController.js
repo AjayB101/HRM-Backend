c45ec63c9ed5a37cc072cdf5503621a95cc36e26
AjayB101 <134268911+AjayB101@users.noreply.github.com>
2023-07-11 14:08:28 +0530

    Ats (#32)
    * file is uploading
    
    * Resume is storing
    
    * photo-resume-added
    
    * resume is downloadind
    
    * photo download added
    
    * fs unlink added
    
    * Revert "fs unlink added"
    
    This reverts commit 110a3ae51653ceb674bf499699d67b946593d046.
    
    * reomoved photo
    
    * unlink added

--- controllers/AtsController.js
+++ controllers/AtsController.js
@@ -1,6 +1,8 @@
 //Controller
 const atsModel = require('../model/Ats');
 const fs = require('fs');
+const path = require('path');
+const { dirname } = require('path/posix');
 const getAts = async (req, res) => {
   try {
     const getData = await atsModel.find({});
@@ -12,12 +14,12 @@ const getAts = async (req, res) => {
     res.status(500).json(error);
   }
 };
-
 const createAts = async (req, res) => {
   const { email, name, phone, position, qualification, college, graduationYear, skills } = req.body;
   const { resume, photo } = req.files;
-  const resumeFile = resume[0]
-  const photoFile = photo[0]
+  const resumeFile = resume[0];
+  const photoFile = photo[0];
+
   try {
     const newAts = new atsModel({
       email,
@@ -26,27 +28,27 @@ const createAts = async (req, res) => {
       position,
       college,
       graduationYear,
-      qualification ,
+      qualification,
       skills,
-      applied: req.body.applied, // Assuming "applied" is a field in the request body
+      applied: req.body.applied,
       resume: {
-        data: fs.readFileSync(resumeFile.path),
-        contentType: resumeFile.mimetype
+        data: fs.readFileSync(resumeFile.path), // Read file data as Buffer
+        contentType: resumeFile.mimetype,
       },
       photo: {
-        data: fs.readFileSync(photoFile.path),
-        contentType: photoFile.mimetype
-      }
-    })
+        data: fs.readFileSync(photoFile.path), // Read file data as Buffer
+        contentType: photoFile.mimetype,
+      },
+    });
     await newAts.save()
     fs.unlinkSync(resumeFile.path)
     fs.unlinkSync(photoFile.path)
     res.status(201).json({ message: `data has been Saved` })
   } catch (error) {
-    res.status(500).json(error)
+    console.log(error)
+    res.status(500).json(error);
   }
 };
-
 const deleteAts = async (req, res) => {
   const { id } = req.params;
   const user = await atsModel.findById(id).exec();
@@ -61,7 +63,6 @@ const deleteAts = async (req, res) => {
     res.status(500).json(error);
   }
 };
-
 const getAtsId = async (req, res) => {
   const { id } = req.params;
   const user = await atsModel.findById(id).exec();
@@ -76,10 +77,66 @@ const getAtsId = async (req, res) => {
     res.status(500).json(error);
   }
 };
-
+const downloadResume = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await atsModel.findById(id).exec();
+    if (!user || !user.resume) {
+      return res.status(404).json({ message: 'Resume not found' });
+    }
+    const file = user.resume;
+    const resumeData = Buffer.from(file.data, 'base64'); // Create Buffer from base64 data
+    const directoryPath = path.join(__dirname, 'download');
+    console.log(`directoryPath = ${directoryPath}`)
+    const fileName = 'resume.pdf';
+    const filePath = path.join(directoryPath, fileName);
+    console.log(`filePath = ${filePath}`)
+    // Create the "download" folder if it doesn't exist
+    if (!fs.existsSync(directoryPath)) {
+      fs.mkdirSync(directoryPath);
+    }
+    fs.writeFileSync(filePath, resumeData); // Write the resume data to a file
+    console.log(`Resume saved at: ${filePath}`);
+    res.set('Content-Type', file.contentType);
+    res.set('Content-Disposition', `attachment; filename="${fileName}"`);
+    res.sendFile(filePath);
+  } catch (error) {
+    console.log(`Error while downloading resume: ${error}`);
+    res.status(500).json(error);
+  }
+};
+const downloadPhoto = async (req, res) => {
+  const { id } = req.params;
+  const user = await atsModel.findById(id).exec();
+  if (!user || !user.photo) {
+    return res.status(404).json({ message: 'Photo not found' });
+  }
+try {
+  const file=user.photo
+  const baseFileConv=Buffer.from(file.data,'base64')
+  const directoryPath=path.join(__dirname,'download')
+  const fileName = 'photo.png';
+  console.log(`directoryPath ${directoryPath}`)
+  const filePath=path.join(directoryPath,fileName)
+  console.log(`filePath ${filePath}`)
+  //*check if dir exist if not new one will be created */
+  if(!fs.existsSync(directoryPath)){
+    fs.mkdir(directoryPath)
+  }
+  fs.writeFileSync(filePath,baseFileConv)
+  res.set('Content-Type', file.contentType);
+  res.set('Content-Disposition', `attachment; filename="${fileName}"`);
+  res.sendFile(filePath)
+} catch (error) {
+  console.log(error)
+  res.status(500).json(error)
+}
+};
 module.exports = {
   getAts,
   createAts,
   deleteAts,
-  getAtsId
+  getAtsId,
+  downloadPhoto,
+  downloadResume
 };
