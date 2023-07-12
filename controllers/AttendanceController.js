8367a971ca4c6cca19291611852ee091577b3f31
Kannan <72759290+Kannan-SN@users.noreply.github.com>
2023-07-12 15:38:50 +0530

    Update AttendanceController.js (#41)

--- controllers/AttendanceController.js
+++ controllers/AttendanceController.js
@@ -6,7 +6,7 @@ exports.checkIn = async (req, res) => {
     const { checkInTime, checkInDate } = req.body;
     const existingAttendance = await Attendance.findOne({ checkInDate });
     if (existingAttendance) {
-      return res.status(400).json({ success: false, message: 'You have already checked in today.' });
+      return res.status(400).json({ error:true, message: 'You have already checked in today.' });
     }
 
     const attendance = new Attendance({ checkInTime, checkInDate });
@@ -14,7 +14,7 @@ exports.checkIn = async (req, res) => {
     res.status(200).json({ success: true, message: 'Check-in recorded successfully.' });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ success: false, message: 'Internal server error' });
+    res.status(500).json({  error:true, message: 'Internal server error' });
   }
 };
 
@@ -24,7 +24,7 @@ exports.checkOut = async (req, res) => {
     const { checkOutTime, checkOutDate } = req.body;
     const existingAttendance = await Attendance.findOne({ checkInDate: checkOutDate });
     if (!existingAttendance || existingAttendance.checkOutTime) {
-      return res.status(400).json({ success: false, message: 'You have not checked in today.' });
+      return res.status(400).json({  error:true, message: 'You have not checked in today.' });
     }
 
     existingAttendance.checkOutTime = checkOutTime;
@@ -33,7 +33,7 @@ exports.checkOut = async (req, res) => {
     res.status(200).json({ success: true, message: 'Check-out recorded successfully.' });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ success: false, message: 'Internal server error' });
+    res.status(500).json({ error:true, message: 'Internal server error' });
   }
 };
 
@@ -44,6 +44,6 @@ exports.getAttendance = async (req, res) => {
     res.status(200).json(attendance);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ success: false, message: 'Internal server error' });
+    res.status(500).json({  error:true, message: 'Internal server error' });
   }
 };
