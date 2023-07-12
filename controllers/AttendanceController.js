2bcaff5e7d617bac99bd3387ca060904a7e6804f
Kannan <72759290+Kannan-SN@users.noreply.github.com>
2023-07-12 13:17:33 +0530

    Update AttendanceController.js (#38)

--- controllers/AttendanceController.js
+++ controllers/AttendanceController.js
@@ -4,6 +4,13 @@ const Attendance = require('../model/AttendanceModel');
 exports.checkIn = async (req, res) => {
   try {
     const { checkInTime, checkInDate } = req.body;
+    const existingAttendance = await Attendance.findOne({ checkInDate });
+    if (existingAttendance && existingAttendance.checkOutTime) {
+      return res.status(400).json({ success: false, message: 'You have already checked in and checked out today.' });
+    } else if (existingAttendance) {
+      return res.status(400).json({ success: false, message: 'You have already checked in today.' });
+    }
+
     const attendance = new Attendance({ checkInTime, checkInDate });
     await attendance.save();
     res.status(200).json({ success: true, message: 'Check-in recorded successfully.' });
@@ -16,14 +23,14 @@ exports.checkIn = async (req, res) => {
 exports.checkOut = async (req, res) => {
   try {
     const { checkOutTime, checkOutDate } = req.body;
-    const attendance = await Attendance.findOneAndUpdate(
-      { checkOutTime: null },
-      { checkOutTime, checkOutDate },
-      { new: true }
-    );
-    if (!attendance) {
-      return res.status(404).json({ success: false, message: 'No active check-in found.' });
+    const existingAttendance = await Attendance.findOne({ checkOutDate });
+    if (!existingAttendance || existingAttendance.checkOutTime) {
+      return res.status(400).json({ success: false, message: 'You have not checked in today.' });
     }
+
+    existingAttendance.checkOutTime = checkOutTime;
+    await existingAttendance.save();
+
     res.status(200).json({ success: true, message: 'Check-out recorded successfully.' });
   } catch (error) {
     console.error(error);
