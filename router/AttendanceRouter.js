bb5183ef6a41ce32cee0a3527998d5803b5c8609
Kannan <72759290+Kannan-SN@users.noreply.github.com>
2023-07-13 15:03:28 +0530

    crud added

--- router/AttendanceRouter.js
+++ router/AttendanceRouter.js
@@ -1,3 +1,5 @@
+// attendanceRoutes.js
+
 const express = require('express');
 const router = express.Router();
 const attendanceController = require('../controllers/AttendanceController');
@@ -5,6 +7,8 @@ const attendanceController = require('../controllers/AttendanceController');
 router.post('/checkin', attendanceController.checkIn);
 router.post('/checkout', attendanceController.checkOut);
 router.get('/', attendanceController.getAttendance);
-
+router.get('/:id', attendanceController.getAttendanceById);
+router.put('/:id', attendanceController.updateAttendance);
+router.delete('/:id', attendanceController.deleteAttendance);
 
 module.exports = router;
