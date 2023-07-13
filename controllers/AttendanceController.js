02e37ec04880deac52dc4748246fb37bf9c3a9c1
Kannan <72759290+Kannan-SN@users.noreply.github.com>
2023-07-13 15:16:55 +0530

    crud removed

--- controllers/AttendanceController.js
+++ controllers/AttendanceController.js
@@ -39,60 +39,9 @@ exports.checkOut = async (req, res) => {
 
 exports.getAttendance = async (req, res) => {
   try {
-    const attendance = await Attendance.find().sort({ _id: -1 });
-    res.status(200).json(attendance);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: true, message: 'Internal server error' });
-  }
-};
-
-exports.getAttendanceById = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const attendance = await Attendance.findById(id);
-    if (!attendance) {
-      return res.status(404).json({ error: true, message: 'Attendance not found' });
-    }
-    res.status(200).json(attendance);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: true, message: 'Internal server error' });
-  }
-};
-
-exports.updateAttendance = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { checkInTime, checkInDate, checkOutTime, checkOutDate } = req.body;
-
-    const attendance = await Attendance.findById(id);
-    if (!attendance) {
-      return res.status(404).json({ error: true, message: 'Attendance not found' });
-    }
-
-    attendance.checkInTime = checkInTime;
-    attendance.checkInDate = checkInDate;
-    attendance.checkOutTime = checkOutTime;
-    attendance.checkOutDate = checkOutDate;
-
-    await attendance.save();
-
-    res.status(200).json({ success: true, message: 'Attendance updated successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: true, message: 'Internal server error' });
-  }
-};
-
-exports.deleteAttendance = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const attendance = await Attendance.findByIdAndDelete(id);
-    if (!attendance) {
-      return res.status(404).json({ error: true, message: 'Attendance not found' });
-    }
-    res.status(200).json({ success: true, message: 'Attendance deleted successfully' });
+    const attendance = await Attendance.findOne().sort({ _id: -1 });
+    const isCheckInDone = attendance && attendance.checkInDate === new Date().toLocaleDateString('en-IN', { timeZone: 'Asia/Kolkata' });
+    res.status(200).json({ attendance, isCheckInDone });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: true, message: 'Internal server error' });
--- router/AttendanceRouter.js
+++ router/AttendanceRouter.js
@@ -1,5 +1,3 @@
-// attendanceRoutes.js
-
 const express = require('express');
 const router = express.Router();
 const attendanceController = require('../controllers/AttendanceController');
@@ -7,8 +5,6 @@ const attendanceController = require('../controllers/AttendanceController');
 router.post('/checkin', attendanceController.checkIn);
 router.post('/checkout', attendanceController.checkOut);
 router.get('/', attendanceController.getAttendance);
-router.get('/:id', attendanceController.getAttendanceById);
-router.put('/:id', attendanceController.updateAttendance);
-router.delete('/:id', attendanceController.deleteAttendance);
+
 
 module.exports = router;
