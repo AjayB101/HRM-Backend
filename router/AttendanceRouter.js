819602861df7d44cf00871c7d1d61f3b630ca2ab
Kannan <72759290+Kannan-SN@users.noreply.github.com>
2023-07-13 14:49:36 +0530

    Update AttendanceRouter.js

--- router/AttendanceRouter.js
+++ router/AttendanceRouter.js
@@ -5,8 +5,6 @@ const attendanceController = require('../controllers/AttendanceController');
 router.post('/checkin', attendanceController.checkIn);
 router.post('/checkout', attendanceController.checkOut);
 router.get('/', attendanceController.getAttendance);
-router.get('/:id', attendanceController.getAttendanceById);
-router.put('/:id', attendanceController.updateAttendance);
-router.delete('/:id', attendanceController.deleteAttendance);
+
 
 module.exports = router;
