2fc4d2b0795f6d9b97e375b8de7afa3288f0e441
Kannan <72759290+Kannan-SN@users.noreply.github.com>
2023-07-13 14:44:46 +0530

    disable fixing

--- controllers/AttendanceController.js
+++ controllers/AttendanceController.js
@@ -1,16 +1,18 @@
+// attendanceController.js
+
 const Attendance = require('../model/AttendanceModel');
 
 exports.checkIn = async (req, res) => {
   try {
     const { checkInTime, checkInDate } = req.body;
     const existingAttendance = await Attendance.findOne({ checkInDate });
-    if (existingAttendance && !existingAttendance.checkOutTime) {
-      return res.status(400).json({ error: true, message: 'You have already checked in today.' });
+    if (existingAttendance) {
+      return res.status(400).json({ error: true, message: 'You have already checked in today.', isCheckInDone: true });
     }
 
     const attendance = new Attendance({ checkInTime, checkInDate });
     await attendance.save();
-    res.status(200).json({ success: true, message: 'Check-in recorded successfully.' });
+    res.status(200).json({ success: true, message: 'Check-in recorded successfully.', isCheckInDone: true });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: true, message: 'Internal server error' });
@@ -22,13 +24,13 @@ exports.checkOut = async (req, res) => {
     const { checkOutTime, checkOutDate } = req.body;
     const existingAttendance = await Attendance.findOne({ checkInDate: checkOutDate });
     if (!existingAttendance || existingAttendance.checkOutTime) {
-      return res.status(400).json({ error: true, message: 'You have not checked in today.' });
+      return res.status(400).json({ error: true, message: 'You have not checked in today.', isCheckInDone: false });
     }
 
     existingAttendance.checkOutTime = checkOutTime;
     await existingAttendance.save();
 
-    res.status(200).json({ success: true, message: 'Check-out recorded successfully.' });
+    res.status(200).json({ success: true, message: 'Check-out recorded successfully.', isCheckInDone: false });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: true, message: 'Internal server error' });
@@ -37,55 +39,9 @@ exports.checkOut = async (req, res) => {
 
 exports.getAttendance = async (req, res) => {
   try {
-    const attendance = await Attendance.find();
-    res.status(200).json(attendance);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: true, message: 'Internal server error' });
-  }
-};
-
-exports.getAttendanceById = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const attendance = await Attendance.findById(id);
-    if (!attendance) {
-      return res.status(404).json({ error: true, message: 'Attendance not found.' });
-    }
-    res.status(200).json(attendance);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: true, message: 'Internal server error' });
-  }
-};
-
-exports.updateAttendance = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { checkInTime, checkOutTime } = req.body;
-    const attendance = await Attendance.findByIdAndUpdate(
-      id,
-      { checkInTime, checkOutTime },
-      { new: true }
-    );
-    if (!attendance) {
-      return res.status(404).json({ error: true, message: 'Attendance not found.' });
-    }
-    res.status(200).json(attendance);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: true, message: 'Internal server error' });
-  }
-};
-
-exports.deleteAttendance = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const attendance = await Attendance.findByIdAndDelete(id);
-    if (!attendance) {
-      return res.status(404).json({ error: true, message: 'Attendance not found.' });
-    }
-    res.status(200).json({ success: true, message: 'Attendance deleted successfully.' });
+    const attendance = await Attendance.findOne().sort({ _id: -1 });
+    const isCheckInDone = attendance && attendance.checkInDate === new Date().toLocaleDateString('en-IN', { timeZone: 'Asia/Kolkata' });
+    res.status(200).json({ attendance, isCheckInDone });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: true, message: 'Internal server error' });
