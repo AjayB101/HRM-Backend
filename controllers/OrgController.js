6a02a5d066016bdeafd7a70c3c61a1cbc1e135d0
AjayB101 <ajayjadec@gmail.com>
2023-08-23 15:16:49 +0530

    delete org

--- controllers/OrgController.js
+++ controllers/OrgController.js
@@ -29,7 +29,33 @@ const createOrg = async (req,res)=>{
     console.log(error) 
   }
 }
+ 
+const deleteOrg = async(req,res)=>{
+  const {id}=req.params
+  if(!id){
+    return res.status(400).json({message:'No Id Is Provided'})
+  }
+  const orgData = await orgModel.findById(id).exec()
+  if(!orgData){
+   return res.status(400).json({message:`No User Has Found Having The Id ${id} Please Provide A Valid Id`})
+  }
+  try{
+     await orgModel.deleteOne({_id:id})
+     .then(()=>{
+      res.status(200).json({message:`Data Has Been Deleted Having The Id ${id}`})
+     })
+     .catch(err=>{
+      console.log(err)
+      return res.status(400).json(err)
+     })
+  }
+  catch(e){
+    console.log(e)
+    res.status(500).json(e)
+  }
+}
 module.exports = {
   getOrgs,
-  createOrg
+  createOrg,
+  deleteOrg,
 };
--- router/OrgRouter.js
+++ router/OrgRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router=express.Router()
-const {getOrgs,createOrg}=require('../controllers/OrgController')
+const {getOrgs,createOrg, deleteOrg}=require('../controllers/OrgController')
 
 
 
 router.get('/getorgs',getOrgs)
 router.post('/createorg',createOrg)
+router.delete('/deleteorg/:id',deleteOrg)
 module.exports=router
