fcf1614e11a5ed8bee9a17792fdce749ed4608ba
AjayB101 <ajayjadec@gmail.com>
2023-09-11 10:42:05 +0530

    isReported Added

--- model/Employee.js
+++ model/Employee.js
@@ -1,94 +1,100 @@
 // models/employeeModel.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const employeeSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  lastname: {
-    type: String,
-    required: true
-  },
-  gender: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  dob: {
-    type: Date,
-    required: true
-  },
-
-
-  mob: {
-    type: Number,
-    required: true
-  },
-  altmob: {
-    type: Number,
-    required: true
-  },
-  dept: {
-    type: String,
-    required: true
-  },
-  desi: {
-    type: String,
-    required: true
-  },
-  peraddress: {
-    type: String,
-    required: true
-  },
-  temaddress: {
-    type: String,
-    required: true
-  },
-  bloodgroup: {
-    type: String,
-    required: true
-  },
-  join: {
-    type: Date,
-    required: true
-  },
-  report:
-[ {
+const employeeSchema = new mongoose.Schema(
+  {
     name: {
-      type: String
+      type: String,
+      required: true,
     },
-    id: {
-      type: String
+    lastname: {
+      type: String,
+      required: true,
     },
-  }],
+    gender: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    dob: {
+      type: Date,
+      required: true,
+    },
 
-  type: {
-    type: String,
-    required: true
-  },
-  employeeid: {
-    type: String,
-    required: true
-  },
-  approval: {
-    manager: {
-      type: Boolean,
-      default: false
+    mob: {
+      type: Number,
+      required: true,
     },
-    hr: {
+    altmob: {
+      type: Number,
+      required: true,
+    },
+    dept: {
+      type: String,
+      required: true,
+    },
+    desi: {
+      type: String,
+      required: true,
+    },
+    peraddress: {
+      type: String,
+      required: true,
+    },
+    temaddress: {
+      type: String,
+      required: true,
+    },
+    bloodgroup: {
+      type: String,
+      required: true,
+    },
+    join: {
+      type: Date,
+      required: true,
+    },
+    report: [
+      {
+        name: {
+          type: String,
+        },
+        id: {
+          type: String,
+        },
+      },
+    ],
+    isReported: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    employeeid: {
+      type: String,
+      required: true,
+    },
+    approval: {
+      manager: {
+        type: Boolean,
+        default: false,
+      },
+      hr: {
+        type: Boolean,
+        default: false,
+      },
+    },
+  },
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true
-});
+);
 
-const Employee = mongoose.model('employee', employeeSchema);
+const Employee = mongoose.model("employee", employeeSchema);
 
 module.exports = Employee;
