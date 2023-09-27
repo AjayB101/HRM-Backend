823157ec7207b2b92010c4ffacb1f0916dc6fba1
AjayB101 <134268911+AjayB101@users.noreply.github.com>
2023-09-27 15:57:44 +0530

    event completed (#64)

--- controllers/EventCtrl.js
+++ controllers/EventCtrl.js
@@ -1,52 +1,108 @@
-const eventModel=require('../model/EventMdl')
+const eventModel = require("../model/EventMdl");
 
-const getAllData=async(req,res)=>{
-    try {
-    const eventDatas=await eventModel.find({})
-    .then(data=>{
-        return res.status(200).json({message:'Data Has Been Fetched From Server',data })
-    }).catch(err=>{
-        return res.status(400).json(err)
-    })
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json(error)
+const getAllData = async (req, res) => {
+  try {
+    const eventDatas = await eventModel
+      .find({})
+      .then((data) => {
+        return res
+          .status(200)
+          .json({ message: "Data Has Been Fetched From Server", data });
+      })
+      .catch((err) => {
+        return res.status(400).json(err);
+      });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
+};
 
+const getDataById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const eventData = await eventModel.findById(id);
+    if (!eventData) {
+      return res
+        .status(400)
+        .json({ message: `No Data Has Been Found With The Id ${id}` });
+    } else {
+      return res
+        .status(200)
+        .json({ message: "The Data Has Been From The Server", eventData });
     }
-}
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
+};
+const createData = async (req, res) => {
+  try {
+    const eventData = new eventModel({
+      ...req.body,
+    });
+    await eventData
+      .save()
+      .then((data) => {
+        return res
+          .status(200)
+          .json({ message: "Data Has Been Stored To The Server", data });
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(400).json(err);
+      });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
+};
 
-const getDataById=async(req,res)=>{
-    const {id}=req.params
-    try {
-        const eventData=await eventModel.findById(id)
-        if(!eventData){
-            return res.status(400).json({message:`No Data Has Been Found With The Id ${id}`})
-        }
-        else{
-            return res.status(200).json({message:'The Data Has Been From The Server',eventData})
-        }
-
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json(error)
+const updateData = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const eventData = await eventModel.findById(id);
+    if (!eventData) {
+      return res
+        .status(400)
+        .json({ message: `No Data Has Been Found With The Id ${id}` });
+    } else {
+      await eventModel
+        .findByIdAndUpdate(id, { $set: req.body }, { new: true })
+        .then((data) => {
+          return res
+            .status(200)
+            .json({ message: `Data Has Been Updated `, data });
+        })
+        .catch((err) => {
+          console.log(err);
+          return res.status(400).json(err);
+        });
     }
-}
-const createData=async(req,res)=>{
-try {
-    const eventData=new eventModel({
-        ...req.body
-    })
-    await eventData.save()
-    .then(data=>{
-        return res.status(200).json({message:'Data Has Been Stored To The Server',data})
-    })
-    .catch(err=>{
-        console.log(err)
-        return res.status(400).json(err)
-    })
-} catch (error) {
-    console.log(error)
-    return res.status(500).json(error)
-} 
-}
-module.exports={getAllData,getDataById,createData} 
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
+};
+
+const deleteData = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const eventData = await eventModel.findById(id);
+    if (!eventData) {
+      return res
+        .status(400)
+        .json({ message: `No Data Has Been Found With The Id ${id}` });
+    } else {
+      await eventModel.findByIdAndDelete(id);
+      return res
+        .status(200)
+        .json({ message: "Data Has Been Deleted From Server" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
+};
+
+module.exports = { getAllData, getDataById, createData, updateData,deleteData };
