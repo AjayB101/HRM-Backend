1c555e27596850ccd7fc09c4136865b821e29f1b
CyberHawk030 <cyber.hawk030@gmail.com>
2023-10-12 16:21:55 +0530

    Squashed commit of the following:
    commit 5ec4eacbb2bb7e6d2caf86ddab6c0c670238f470
    Author: CyberHawk030 <cyber.hawk030@gmail.com>
    Date:   Thu Oct 12 15:57:36 2023 +0530
    
        push

--- model/GoalTask.js
+++ model/GoalTask.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const GoalTask = new mongoose.Schema({
   title: {
     type: String,
-    required:true
+
     
   },
   description: {
     type: String,
-    required:true
+
     
   },
   goalid: {
