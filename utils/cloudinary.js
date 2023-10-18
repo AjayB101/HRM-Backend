dde5d478dce7c795c0c8557c7cf7f22eaa7f9a4d
AjayB101 <ajayjadec@gmail.com>
2023-10-18 15:16:35 +0530

    delet

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,20 +1,44 @@
-const cloudinary =  require('cloudinary').v2
+const cloudinary = require("cloudinary").v2;
 
-exports.uploads=(file,folder)=>{
-    return new Promise((resolve,reject)=>{
-        cloudinary.uploader.upload(file,{
-            resource_type:'image',
-            folder:folder
-        },(error,result)=>{
-            if(error){
-                reject(error)
-            }
-            else{
-                resolve({
-                    public_id:result.public_id,
-                    url:result.url
-                })
-            }
-        })
-    })
-}
+exports.uploads = (file, folder) => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(
+      file,
+      {
+        resource_type: "image",
+        folder: folder,
+      },
+      (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve({
+            public_id: result.public_id,
+            url: result.url,
+          });
+        }
+      }
+    );
+  });
+};
+
+exports.cloudinaryDeleteImg = (async(pubId) => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.destroy(pubId, (error, result) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(
+          {
+            url: result.secure_url,
+            asset_id: result.asset_id,
+            public_id: result.public_id,
+          },
+          {
+            resource_type: "auto",
+          }
+        );
+      }
+    });
+  });
+});
