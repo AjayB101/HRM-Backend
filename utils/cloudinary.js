d66b05965f2b5583443142637609c601c6498878
AjayB101 <134268911+AjayB101@users.noreply.github.com>
2023-10-18 12:38:16 +0530

    Account settings  (#72)
    * updated code
    
    * Update EmployeeController.js

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,18 +1,20 @@
 const cloudinary =  require('cloudinary').v2
-exports.uploads = (file) => {
-    return new Promise((resolve, reject) => {
-        cloudinary.uploader.upload(file, {
-            resource_type: "image",
-            folder: 'profile',
-        }, (error, result) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve({
-                    public_id: result.public_id,
-                    url: result.url
-                });
+
+exports.uploads=(file)=>{
+    return new Promise((resolve,reject)=>{
+        cloudinary.uploader.upload(file,{
+            resource_type:'image',
+            folder:'profile'
+        },(error,result)=>{
+            if(error){
+                reject(error)
             }
-        });
-    });
-};
+            else{
+                resolve({
+                    public_id:result.public_id,
+                    url:result.url
+                })
+            }
+        })
+    })
+}
