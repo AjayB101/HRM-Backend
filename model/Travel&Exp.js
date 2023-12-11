fdeb8c9cd88d1f77c3664d7b4053b7b78bae890e
SanjayAnbu22 <134280780+sanjayanbu12@users.noreply.github.com>
2023-12-11 10:35:27 +0530

    travel

--- model/Travel&Exp.js
+++ model/Travel&Exp.js
@@ -24,7 +24,7 @@ const travelExpSchema=new mongoose.Schema({
         type:Number,
         required:true
     },
-    buisness:{
+    business:{
         type:String,
         required:true
     },
